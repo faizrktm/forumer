653267148abb4c1ba0ab995f8927a8d66065957a
Full Stuck Developer <faiz@yukstay.com>
2020-06-26 19:41:57 +0700

    create base Page component

---
+++ src/components/Page.jsx
@@ -0,0 +1,38 @@
+import Head from 'next/head';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+
+import Header from './Header';
+
+const Page = ({ children, title }) => (
+  <>
+    <Head>
+      <title>{title}</title>
+    </Head>
+    <Header />
+    <Main>
+      {children}
+    </Main>
+  </>
+);
+
+Page.defaultProps = {
+  title: 'Forumer',
+};
+
+Page.propTypes = {
+  children: PropTypes.node.isRequired,
+  title: PropTypes.string,
+};
+
+export default Page;
+
+const Main = styled.main`
+  position: relative;
+  top: 74px;
+  min-height: calc(100vh - 74px);
+  margin: 0px auto;
+  padding: 14px 16px 32px;
+  background-color: transparent;
+  width: 100%;
+`;
