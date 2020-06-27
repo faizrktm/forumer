c74571d1058c40f0d5e140c9c60e8cbc7a51a999
Full Stuck Developer <faiz@yukstay.com>
2020-06-27 17:16:46 +0700

    Create simple state management

---
+++ src/components/Reduxer.jsx
@@ -0,0 +1,91 @@
+import { useReducer } from 'react';
+import PropTypes from 'prop-types';
+
+const ReduxerContext = React.createContext({ data: {} });
+
+function reducer(state, action) {
+  const { type, payload } = action;
+  switch (type) {
+    case 'REQUEST':
+      return ({
+        ...state,
+        status: 'loading',
+      });
+    case 'SUCCESS':
+      return ({
+        status: 'idle',
+        data: payload,
+      });
+    case 'FAILURE':
+      return ({
+        ...state,
+        status: payload,
+      });
+    default:
+      return state;
+  }
+}
+
+const ReduxerProvider = ({ children, initialValue }) => {
+  const [state, dispatch] = useReducer(reducer, { status: 'idle', data: initialValue });
+  const { data, status } = state;
+
+  /**
+   *
+   * @param {() => object} handler
+   * handler must return single object that does not a list. e.g. {...data}
+   */
+  const update = async (handler) => {
+    try {
+      dispatch({ type: 'REQUEST' });
+      const response = await handler();
+      const payload = Object.keys(data).reduce((acc, curr) => {
+        if (curr === response.id) {
+          return ({ ...acc, [curr]: response });
+        }
+        return ({ ...acc, [curr]: data[curr] });
+      }, {});
+      dispatch({ type: 'SUCCESS', payload });
+    } catch (error) {
+      dispatch({ type: 'FAILURE', payload: error.message });
+    }
+  };
+
+  /**
+   *
+   * @param {() => object} handler
+   * handler must return object that contain id, eg: {[id]: data}
+   */
+  const add = async (handler) => {
+    try {
+      dispatch({ type: 'REQUEST' });
+      const response = await handler();
+      dispatch({ type: 'SUCCESS', payload: { ...response, ...data } });
+    } catch (error) {
+      dispatch({ type: 'FAILURE', payload: error.message });
+    }
+  };
+
+  return (
+    <ReduxerContext.Provider value={{
+      data,
+      add,
+      update,
+      status,
+    }}
+    >
+      {children}
+    </ReduxerContext.Provider>
+  );
+};
+
+ReduxerProvider.defaultProps = {
+  initialValue: null,
+};
+
+ReduxerProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+  initialValue: PropTypes.oneOfType([PropTypes.object]),
+};
+
+export { ReduxerProvider, ReduxerContext };
