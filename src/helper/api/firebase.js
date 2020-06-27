b4a167136d527241b7adec33f81f439a413707d8
Full Stuck Developer <faiz@yukstay.com>
2020-06-27 11:48:46 +0700

    reconfigure firebase sdk initialization

---
+++ src/helper/api/firebase.js
@@ -0,0 +1,29 @@
+import firebaseApp from 'firebase/app';
+
+import 'firebase/auth';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+};
+
+try {
+  firebaseApp.initializeApp(firebaseConfig);
+} catch (error) {
+  /*
+   * We skip the "already exists" message which is
+   * not an actual error when we're hot-reloading.
+   */
+  if (!/already exists/u.test(error.message)) {
+    // eslint-disable-next-line no-console
+    console.error('Firebase initialization error', error.stack);
+  }
+}
+
+export default firebaseApp;
