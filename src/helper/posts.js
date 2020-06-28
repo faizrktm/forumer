2f957f952e6e52619543a76f111e26234b5d8152
Full Stuck Developer <faiz@yukstay.com>
2020-06-28 15:42:08 +0700

    create helper for add comment

--- src/helper/posts.js
+++ src/helper/posts.js
@@ -21,3 +21,30 @@ export async function addPost(payload, token) {
     return Promise.reject(error);
   }
 }
+
+export async function addComment(payload, token, post) {
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: JSON.stringify(token),
+    },
+    body: JSON.stringify(payload),
+  };
+  try {
+    const response = await fetch(config.API.POSTS, options);
+    const json = await response.json();
+    if (json.code !== 200) {
+      throw new Error(json.result.message);
+    }
+    return {
+      ...post,
+      comments: {
+        ...post.comments,
+        [json.result.id]: json.result,
+      },
+    };
+  } catch (error) {
+    return Promise.reject(error);
+  }
+}
