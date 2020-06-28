2d2aecd4178f69e4cfaf0c38ed7bc490a40a2f65
Full Stuck Developer <faiz@yukstay.com>
2020-06-28 15:49:58 +0700

    update api uri base path

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,4 @@
-export const server = process.env.NODE_ENV !== 'production' ? 'http://localhost:3000' : 'http://localhost:3000';
+export const server = process.env.NODE_ENV !== 'production' ? 'https://uneug-o9k9689ov.vercel.app' : 'http://localhost:3000';
 export default {
   TOKEN_COOKIES_NAME: 'GoogleIDToken',
   API: {
