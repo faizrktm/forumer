708a909fa6f94a8e91819a6951e8e5c126f6220b
Full Stuck Developer <faiz@yukstay.com>
2020-06-30 21:25:23 +0700

    optimize status wrapper

--- src/components/StatusWrapper.jsx
+++ src/components/StatusWrapper.jsx
@@ -7,12 +7,15 @@ const StatusProvider = ({ children }) => {
   const [status, setStatus] = useState('idle');
 
   const setLoading = () => {
+    if (status === 'loading') return;
     setStatus('loading');
   };
   const setIdle = () => {
+    if (status === 'idle') return;
     setStatus('idle');
   };
   const setError = (message) => {
+    if (status === message) return;
     setStatus(message);
   };
 
