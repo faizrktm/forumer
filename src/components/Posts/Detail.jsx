c2e8907856b7acae1e802c86a9fae80fae56df52
Full Stuck Developer <faiz@yukstay.com>
2020-07-08 10:11:41 +0700

    create detail page

---
+++ src/components/Posts/Detail.jsx
@@ -0,0 +1,32 @@
+import { memo, useContext } from 'react';
+import { ReduxerContext, ReduxerProvider } from 'components/Reduxer';
+import { Box } from 'grommet';
+import { timestampToHumans } from 'helper/times';
+
+import config from 'config';
+import Comments from 'components/Comments';
+import PostCard from './PostCard';
+
+const Post = () => {
+  const { data } = useContext(ReduxerContext);
+
+  if (!data) return null;
+
+  return (
+    <Box gap="medium" margin={{ top: 'medium' }}>
+      <PostCard
+        key={data.id}
+        id={data.id}
+        content={data.content}
+        name={data.user.name}
+        time={timestampToHumans(data.timestamp._seconds)}
+        totalComments={data.total_comments}
+      />
+      <ReduxerProvider uri={`${config.API.POSTS}/${data.id}/comments`}>
+        <Comments reference={data.id} totalComments={data.total_comments} />
+      </ReduxerProvider>
+    </Box>
+  );
+};
+
+export default memo(Post);
